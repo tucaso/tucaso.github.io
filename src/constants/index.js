210a414669d48c6251c93c438d58446051474506
jvelezpo <sebasvelez@gmail.com>
2020-05-22 11:36:55 -0500

    build

--- src/constants/index.js
+++
@@ -1,5 +0,0 @@
-export * from './settings';
-export * from './subscription-status';
-export * from './payment-types';
-export * from './payment-status';
-export * from './tracking';
--- src/constants/payment-types.js
+++
@@ -1,3 +0,0 @@
-export const PAYMENT_TYPE_TRANSFER = 'transfer';
-export const PAYMENT_TYPE_RECURRENT = 'recurrent';
-export const PAYMENT_TYPE_STRIPE = 'stripe';
